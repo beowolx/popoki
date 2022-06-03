ecbb37133ae35aa796d75bef64359e793a92ae99
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2022-06-03 15:55:31 +0200

    feat: first naive implementation and tests for it

--- src/algorithms/naive.rs
+++ src/algorithms/naive.rs
@@ -1,9 +1,21 @@
-use crate::{Correctness, Guess, Guesser};
+use crate::{Guess, Guesser};
 
 pub struct Naive;
 
+impl Naive {
+    pub fn new() -> Self {
+        Naive
+    }
+}
+
+impl Default for Naive {
+    fn default() -> Self {
+        Naive::new()
+    }
+}
+
 impl Guesser for Naive {
-    fn guess(&mut self, history: &[Guess]) -> String {
+    fn guess(&mut self, _history: &[Guess]) -> String {
         todo!();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 pub mod algorithms;
 
-/// It takes a guesser and an answer, and returns an `Option` with the number of guesses it took to
-/// guess the answer
 pub fn play<G: Guesser>(answer: &'static str, mut guesser: G) -> Option<usize> {
     let mut history = Vec::new();
 
     // Wordle only allows six guesses.
     // Popoki allows more to avoid chopping off the score distribution for stats
     // purposes.
-    for i in 1..32 {
+    for i in 1..=32 {
         let guess = guesser.guess(&history);
         if guess == answer {
             return Some(i);
@@ -21,7 +19,7 @@ pub fn play<G: Guesser>(answer: &'static str, mut guesser: G) -> Option<usize> {
     }
     None
 }
-
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Correctness {
     // Green
     Correct,
@@ -35,7 +33,41 @@ impl Correctness {
     /// Given an answer and a guess, return an array of 5 elements, each of which is
     /// a `Result` indicating whether the guess is correct, incorrect, or not present
     pub fn compute(answer: &str, guess: &str) -> [Self; 5] {
-        todo!()
+        assert_eq!(answer.len(), 5);
+        assert_eq!(guess.len(), 5);
+        let mut c = [Correctness::Wrong; 5];
+
+        // Mark things green
+        for (i, item) in c.iter_mut().enumerate() {
+            if answer.chars().nth(i) == guess.chars().nth(i) {
+                *item = Correctness::Correct;
+            }
+        }
+
+        let mut used = [false; 5];
+        for (i, c) in c.iter().enumerate() {
+            if *c == Correctness::Correct {
+                used[i] = true;
+            }
+        }
+
+        // Mark things yellow
+        for (i, g) in guess.chars().enumerate() {
+            if c[i] == Correctness::Correct {
+                // Already marked as green
+                continue;
+            }
+            if answer.chars().enumerate().any(|(i, a)| {
+                if a == g && !used[i] {
+                    used[i] = true;
+                    return true;
+                }
+                false
+            }) {
+                c[i] = Correctness::Misplaced;
+            }
+        }
+        c
     }
 }
 
@@ -45,7 +77,66 @@ pub struct Guess {
 }
 
 pub trait Guesser {
-    /// A trait method that takes a mutable reference to self and a reference to a
-    /// slice of Guesses and returns a String.
     fn guess(&mut self, history: &[Guess]) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    mod compute {
+        use crate::Correctness;
+
+        macro_rules! mask {
+            (C) => {Correctness::Correct};
+            (M) => {Correctness::Misplaced};
+            (W) => {Correctness::Wrong};
+            ($($c:tt)+) => {[
+                $(mask!($c)),+
+            ]}
+        }
+
+        #[test]
+        fn all_green() {
+            assert_eq!(Correctness::compute("abcde", "abcde"), mask![C C C C C]);
+        }
+
+        #[test]
+        fn all_gray() {
+            assert_eq!(Correctness::compute("abcde", "fghij"), mask![W W W W W]);
+        }
+
+        #[test]
+        fn all_yellow() {
+            assert_eq!(Correctness::compute("abcde", "eabcd"), mask![M M M M M]);
+        }
+
+        #[test]
+        fn repeat_green() {
+            assert_eq!(Correctness::compute("aabbb", "aaccc"), mask![C C W W W]);
+        }
+
+        #[test]
+        fn repeat_yellow() {
+            assert_eq!(Correctness::compute("aabbb", "ccaac"), mask![W W M M W]);
+        }
+
+        #[test]
+        fn repeat_some_green() {
+            assert_eq!(Correctness::compute("aabbb", "caacc"), mask![W C M W W]);
+        }
+
+        #[test]
+        fn only_one_yellow() {
+            assert_eq!(Correctness::compute("azzaz", "aaabb"), mask![C M W W W]);
+        }
+
+        #[test]
+        fn only_one_green() {
+            assert_eq!(Correctness::compute("baccc", "aaddd"), mask![W C W W W]);
+        }
+
+        #[test]
+        fn only_one_gray() {
+            assert_eq!(Correctness::compute("abcde", "aacde"), mask![C W C C C]);
+        }
+    }
 }
