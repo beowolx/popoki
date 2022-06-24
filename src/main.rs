3821acc5619ac86ddb202d0fc97f94c7d540da4d
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2022-06-24 08:04:47 +0000

    feat: implement CLI args for implementation and games max

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,50 @@
     clippy::option_if_let_else
 )]
 
+use clap::{Parser, ValueEnum};
+use popoki::Guesser;
+
 mod algorithms;
 
 const GAMES: &str = include_str!("../answers.txt");
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The type of algorithm implementation that the user wants to use
+    #[clap(short, long, value_enum, default_value_t = Implementation::Naive)]
+    implementation: Implementation,
+
+    /// The number of games that we want run our algorithm implementation
+    //TODO: Perhaps define a limit of games?
+    #[clap(
+        help = "The number of games that we want run our algorithm implementation",
+        short,
+        long,
+        default_value_t = 5
+    )]
+    max: usize,
+}
+
+#[derive(Parser, ValueEnum, Clone)]
+enum Implementation {
+    Naive,
+}
+
 fn main() {
+    let args = Args::parse();
+    match args.implementation {
+        Implementation::Naive => play(algorithms::Naive::new, args.max),
+    }
+}
+
+fn play<G>(mut mk: impl FnMut() -> G, max: usize)
+where
+    G: Guesser,
+{
     let w = popoki::Wordle::new();
-    for answer in GAMES.split_whitespace() {
-        let guesser = algorithms::Naive::new();
+    for answer in GAMES.split_whitespace().take(max) {
+        let guesser = (mk)();
 
         if let Some(score) = w.play(answer, guesser) {
             println!("Guessed '{answer}' in {score}");
